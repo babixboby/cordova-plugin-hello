2369853b0ef6f668eab655f5c67e4f5ef2ddb5b3
babixboby <babixboby@gmail.com>
2020-04-13 06:24:06 +0700

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -1,58 +1,35 @@
 package com.example.plugin;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.PixelFormat;
-import android.graphics.RectF;
-import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
-import android.util.AttributeSet;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
+import android.content.Intent;
 
-import org.apache.cordova.*;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaInterface;
+
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Hello extends CordovaPlugin {
 
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+
     @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
-        if (action.equals("greet")) {
-
-           // String name = data.getString(0);
-           // String message = "Hello, " + name;
-           // callbackContext.success(message);
-
-            cordova.getActivity().runOnUiThread(new Runnable() {
-                public void run() {
-                    SurfaceView surfaceView = new SurfaceView(activity);
-                    FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        Gravity.CENTER);
-
-                    activity.addContentView(surfaceView, params);
-
-                    callbackContext.success(); 
-                }
-            });
-            
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Context context = cordova.getActivity().getApplicationContext();
+        if(action.equals("new_activity")) {
+            this.openNewActivity(context);
             return true;
-
-        } else {
-            
-            return false;
-
         }
+        return false;
+    }
+
+    private void openNewActivity(Context context) {
+        Intent intent = new Intent(context, NewActivity.class);
+        this.cordova.getActivity().startActivity(intent);
     }
 }
