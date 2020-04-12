3a36ad074e676f66586b4e3ad2af774eea07909d
babixboby <babixboby@gmail.com>
2020-04-12 14:40:23 +0700

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -1,5 +1,23 @@
 package com.example.plugin;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -11,10 +29,24 @@ public class Hello extends CordovaPlugin {
 
         if (action.equals("greet")) {
 
-            String name = data.getString(0);
-            String message = "Hello, " + name;
-            callbackContext.success(message);
+           // String name = data.getString(0);
+           // String message = "Hello, " + name;
+           // callbackContext.success(message);
 
+            cordova.getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                    SurfaceView surfaceView = new SurfaceView(activity);
+                    FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        Gravity.CENTER);
+
+                    activity.addContentView(surfaceView, params);
+
+                    callbackContext.success(); 
+                }
+            });
+            
             return true;
 
         } else {
