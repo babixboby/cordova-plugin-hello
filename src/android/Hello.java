e71d45d01a0ca2f37ca746e544676b564fc654ea
babixboby <babixboby@gmail.com>
2020-04-14 05:57:34 +0700

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -17,7 +17,6 @@ public class Hello extends CordovaPlugin {
 
     public static final String EXTRA_IS_SPEED_MODE = "is_speed_mode";
     Resources activityRes = cordova.getActivity().getResources();
-    public String packageName = cordova.getActivity().getPackageName();
     
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
@@ -33,17 +32,22 @@ public class Hello extends CordovaPlugin {
         return false;
     }
 
+    private String packageName(){
+        String currentPackage = cordova.getActivity().getPackageName();
+        return currentPackage;
+    }
+    
     public static int[] fonts(){
-        int R_font_0 = activityRes.getIdentifier("font_0", "mipmap", packageName);
-        int R_font_1 = activityRes.getIdentifier("font_1", "mipmap", packageName);
-        int R_font_2 = activityRes.getIdentifier("font_2", "mipmap", packageName);
-        int R_font_3 = activityRes.getIdentifier("font_3", "mipmap", packageName);
-        int R_font_4 = activityRes.getIdentifier("font_4", "mipmap", packageName);
-        int R_font_5 = activityRes.getIdentifier("font_5", "mipmap", packageName);
-        int R_font_6 = activityRes.getIdentifier("font_6", "mipmap", packageName);
-        int R_font_7 = activityRes.getIdentifier("font_7", "mipmap", packageName);
-        int R_font_8 = activityRes.getIdentifier("font_8", "mipmap", packageName);
-        int R_font_9 = activityRes.getIdentifier("font_9", "mipmap", packageName);
+        int R_font_0 = activityRes.getIdentifier("font_0", "mipmap", packageName());
+        int R_font_1 = activityRes.getIdentifier("font_1", "mipmap", packageName());
+        int R_font_2 = activityRes.getIdentifier("font_2", "mipmap", packageName());
+        int R_font_3 = activityRes.getIdentifier("font_3", "mipmap", packageName());
+        int R_font_4 = activityRes.getIdentifier("font_4", "mipmap", packageName());
+        int R_font_5 = activityRes.getIdentifier("font_5", "mipmap", packageName());
+        int R_font_6 = activityRes.getIdentifier("font_6", "mipmap", packageName());
+        int R_font_7 = activityRes.getIdentifier("font_7", "mipmap", packageName());
+        int R_font_8 = activityRes.getIdentifier("font_8", "mipmap", packageName());
+        int R_font_9 = activityRes.getIdentifier("font_9", "mipmap", packageName());
         
         int[] A = {R_font_0,R_font_1,R_font_2,R_font_3,R_font_4,R_font_5,R_font_6,R_font_7,R_font_8,R_font_9};
         return A;
