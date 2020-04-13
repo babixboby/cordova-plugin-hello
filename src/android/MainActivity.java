c805eb7887c865ab1385167c658ebcfab11e7426
babixboby <babixboby@gmail.com>
2020-04-14 06:55:45 +0700

    Update and rename NewActivity.java to MainActivity.java

---
+++ src/android/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.plugin;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class MainActivity extends Activity {
+
+    public static final String EXTRA_IS_SPEED_MODE = "is_speed_mode";
+    private ProgressBar mBar;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        String package_name = getApplication().getPackageName();
+        setContentView(getApplication().getResources().getIdentifier("activity_new", "layout", package_name));
+        
+        mBar = (ProgressBar)findViewById(R.id.pb_loading);
+
+        findViewById(R.id.btn_normal_mode).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBar.setVisibility(View.VISIBLE);
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                intent.putExtra(EXTRA_IS_SPEED_MODE, false);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn_speed_mode).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBar.setVisibility(View.VISIBLE);
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                intent.putExtra(EXTRA_IS_SPEED_MODE, true);
+                startActivity(intent);
+            }
+        });
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onPause();
+        mBar.setVisibility(View.INVISIBLE);
+    }
+}
