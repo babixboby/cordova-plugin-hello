3fe247a471af8f2092bded2adc9701ccb4e39908
babixboby <babixboby@gmail.com>
2020-04-14 05:35:55 +0700

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -15,7 +15,17 @@ import org.json.JSONObject;
 public class Hello extends CordovaPlugin {
 
     public static final String EXTRA_IS_SPEED_MODE = "is_speed_mode";
-    
+    public static final int R_font_0 = cordova.getActivity().getResources().getIdentifier("font_0", "mipmap", cordova.getActivity().getPackageName());
+    public static final int R_font_1 = cordova.getActivity().getResources().getIdentifier("font_1", "mipmap", cordova.getActivity().getPackageName());
+    public static final int R_font_2 = cordova.getActivity().getResources().getIdentifier("font_2", "mipmap", cordova.getActivity().getPackageName());
+    public static final int R_font_3 = cordova.getActivity().getResources().getIdentifier("font_3", "mipmap", cordova.getActivity().getPackageName());
+    public static final int R_font_4 = cordova.getActivity().getResources().getIdentifier("font_4", "mipmap", cordova.getActivity().getPackageName());
+    public static final int R_font_5 = cordova.getActivity().getResources().getIdentifier("font_5", "mipmap", cordova.getActivity().getPackageName());
+    public static final int R_font_6 = cordova.getActivity().getResources().getIdentifier("font_6", "mipmap", cordova.getActivity().getPackageName());
+    public static final int R_font_7 = cordova.getActivity().getResources().getIdentifier("font_7", "mipmap", cordova.getActivity().getPackageName());
+    public static final int R_font_8 = cordova.getActivity().getResources().getIdentifier("font_8", "mipmap", cordova.getActivity().getPackageName());
+    public static final int R_font_9 = cordova.getActivity().getResources().getIdentifier("font_9", "mipmap", cordova.getActivity().getPackageName());
+
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
     }
