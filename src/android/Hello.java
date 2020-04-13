875e41bebaaaa30005a9a2226fb4fd07a367d278
babixboby <babixboby@gmail.com>
2020-04-14 05:47:42 +0700

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -15,16 +15,6 @@ import org.json.JSONObject;
 public class Hello extends CordovaPlugin {
 
     public static final String EXTRA_IS_SPEED_MODE = "is_speed_mode";
-    public int R_font_0 = cordova.getActivity().getResources().getIdentifier("font_0", "mipmap", cordova.getActivity().getPackageName());
-    public int R_font_1 = cordova.getActivity().getResources().getIdentifier("font_1", "mipmap", cordova.getActivity().getPackageName());
-    public int R_font_2 = cordova.getActivity().getResources().getIdentifier("font_2", "mipmap", cordova.getActivity().getPackageName());
-    public int R_font_3 = cordova.getActivity().getResources().getIdentifier("font_3", "mipmap", cordova.getActivity().getPackageName());
-    public int R_font_4 = cordova.getActivity().getResources().getIdentifier("font_4", "mipmap", cordova.getActivity().getPackageName());
-    public int R_font_5 = cordova.getActivity().getResources().getIdentifier("font_5", "mipmap", cordova.getActivity().getPackageName());
-    public int R_font_6 = cordova.getActivity().getResources().getIdentifier("font_6", "mipmap", cordova.getActivity().getPackageName());
-    public int R_font_7 = cordova.getActivity().getResources().getIdentifier("font_7", "mipmap", cordova.getActivity().getPackageName());
-    public int R_font_8 = cordova.getActivity().getResources().getIdentifier("font_8", "mipmap", cordova.getActivity().getPackageName());
-    public int R_font_9 = cordova.getActivity().getResources().getIdentifier("font_9", "mipmap", cordova.getActivity().getPackageName());
 
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
@@ -40,6 +30,21 @@ public class Hello extends CordovaPlugin {
         return false;
     }
 
+    public static int[] fonts(){
+        int R_font_0 = cordova.getActivity().getResources().getIdentifier("font_0", "mipmap", cordova.getActivity().getPackageName());
+        int R_font_1 = cordova.getActivity().getResources().getIdentifier("font_1", "mipmap", cordova.getActivity().getPackageName());
+        int R_font_2 = cordova.getActivity().getResources().getIdentifier("font_2", "mipmap", cordova.getActivity().getPackageName());
+        int R_font_3 = cordova.getActivity().getResources().getIdentifier("font_3", "mipmap", cordova.getActivity().getPackageName());
+        int R_font_4 = cordova.getActivity().getResources().getIdentifier("font_4", "mipmap", cordova.getActivity().getPackageName());
+        int R_font_5 = cordova.getActivity().getResources().getIdentifier("font_5", "mipmap", cordova.getActivity().getPackageName());
+        int R_font_6 = cordova.getActivity().getResources().getIdentifier("font_6", "mipmap", cordova.getActivity().getPackageName());
+        int R_font_7 = cordova.getActivity().getResources().getIdentifier("font_7", "mipmap", cordova.getActivity().getPackageName());
+        int R_font_8 = cordova.getActivity().getResources().getIdentifier("font_8", "mipmap", cordova.getActivity().getPackageName());
+        int R_font_9 = cordova.getActivity().getResources().getIdentifier("font_9", "mipmap", cordova.getActivity().getPackageName());
+        
+        int[] A = {R_font_0,R_font_1,R_font_2,R_font_3,R_font_4,R_font_5,R_font_6,R_font_7,R_font_8,R_font_9};
+        return A;
+    }
     private void openNewActivity(Context context) {
         //Intent intent = new Intent(context, NewActivity.class);
         //this.cordova.getActivity().startActivity(intent);
