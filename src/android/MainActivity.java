2ecd16169ffcfecf2809d1fa450900832f063a61
babixboby <babixboby@gmail.com>
2020-04-14 07:13:26 +0700

    Update MainActivity.java

--- src/android/MainActivity.java
+++ src/android/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
-import android.widget.ProgressBar;
 
 public class MainActivity extends Activity {
 
@@ -19,32 +18,32 @@ public class MainActivity extends Activity {
         String package_name = getApplication().getPackageName();
         setContentView(getApplication().getResources().getIdentifier("activity_new", "layout", package_name));
         
-        mBar = (ProgressBar)findViewById(R.id.pb_loading);
+       // mBar = (ProgressBar)findViewById(R.id.pb_loading);
 
         findViewById(R.id.btn_normal_mode).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mBar.setVisibility(View.VISIBLE);
-                Intent intent = new Intent(MainActivity.this, GameActivity.class);
-                intent.putExtra(EXTRA_IS_SPEED_MODE, false);
-                startActivity(intent);
+                //mBar.setVisibility(View.VISIBLE);
+                //Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                //intent.putExtra(EXTRA_IS_SPEED_MODE, false);
+               // startActivity(intent);
             }
         });
 
         findViewById(R.id.btn_speed_mode).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mBar.setVisibility(View.VISIBLE);
-                Intent intent = new Intent(MainActivity.this, GameActivity.class);
-                intent.putExtra(EXTRA_IS_SPEED_MODE, true);
-                startActivity(intent);
+               // mBar.setVisibility(View.VISIBLE);
+                //Intent intent = new Intent(MainActivity.this, GameActivity.class);
+               // intent.putExtra(EXTRA_IS_SPEED_MODE, true);
+                //startActivity(intent);
             }
         });
     }
     
     @Override
     protected void onResume() {
-        super.onPause();
-        mBar.setVisibility(View.INVISIBLE);
+       // super.onPause();
+       // mBar.setVisibility(View.INVISIBLE);
     }
 }
